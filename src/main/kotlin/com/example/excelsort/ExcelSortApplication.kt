13d0ed43e1f8e4f998ef93bdf3d4d3374b9d1862
Filip <skalny.filip@gmail.com>
2025-12-11 19:34:57 +0100

    feat: first commit

---
+++ src/main/kotlin/com/example/excelsort/ExcelSortApplication.kt
@@ -0,0 +1,11 @@
+package com.example.excelsort
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelSortApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExcelSortApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/excelsort/ExcelSortApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.excelsort
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelSortApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
